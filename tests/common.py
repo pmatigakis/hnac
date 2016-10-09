d173bee04c13193f1fa503ebf155f5b085d4714d
Panagiotis Matigakis <pmatigakis@gmail.com>
2016-10-09 12:43:53 +0300

    Cleaned up the model unit tests

---
+++ tests/common.py
@@ -0,0 +1,69 @@
+from datetime import datetime, timedelta
+from unittest import TestCase
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.exc import SQLAlchemyError
+
+from hnac.models import Base, User, Report
+from hnac import jobs
+
+
+class ModelTestCase(TestCase):
+    def setUp(self):
+        self.engine = create_engine("sqlite:///:memory:")
+
+        Base.metadata.create_all(self.engine)
+
+        self.Session = scoped_session(sessionmaker(bind=self.engine))
+
+        self.session = self.Session()
+
+    def tearDown(self):
+        self.session.close()
+
+        self.Session.remove()
+
+
+class ModelTestCaseWithMockData(ModelTestCase):
+    def setUp(self):
+        super(ModelTestCaseWithMockData, self).setUp()
+
+        session = self.Session()
+
+        self.test_user_username = "user1"
+        self.test_user_password = "password"
+
+        user = User.create(session, self.test_user_username,
+                           self.test_user_password)
+
+        job_1 = jobs.Job(None, None, None)
+        job_1.processed_item_count = 5
+        job_1.failed = True
+        job_1.id = "job_1_uuid"
+        job_1_start_time = datetime(2016, 5, 6, 12, 0, 0)
+        job_1_end_time = job_1_start_time + timedelta(seconds=40)
+        report_1 = jobs.Report(job_1, job_1_start_time, job_1_end_time)
+
+        Report.save_report(self.session, report_1)
+
+        job_2 = jobs.Job(None, None, None)
+        job_2.processed_item_count = 11
+        job_2.failed = False
+        job_2.id = "job_2_uuid"
+        job_2_start_time = datetime(2016, 5, 6, 15, 0, 0)
+        job_2_end_time = job_1_start_time + timedelta(seconds=40)
+        report_2 = jobs.Report(job_2, job_2_start_time, job_2_end_time)
+
+        Report.save_report(self.session, report_2)
+
+        try:
+            session.commit()
+
+            self.test_user_id = user.id
+            self.test_user_jti = user.jti
+        except SQLAlchemyError:
+            session.rollback()
+            self.fail("failed to load mock data")
+
+        session.close()
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,32 +1,25 @@
 from datetime import datetime, timedelta
-from unittest import TestCase, main
+from unittest import main
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.exc import SQLAlchemyError
 
-from hnac.models import Base, User, Report as Report
+from hnac.models import User, Report as Report
 from hnac import jobs
 
+from common import ModelTestCase, ModelTestCaseWithMockData
 
-class UserCreationTests(TestCase):
-    def setUp(self):
-        self.engine = create_engine("sqlite:///:memory:")
 
-        Base.metadata.create_all(self.engine)
-
-        self.Session = scoped_session(sessionmaker(bind=self.engine))
-
-    def tearDown(self):
-        self.Session.remove()
-
+class UserCreationTests(ModelTestCase):
     def test_create_api_user(self):
-        session = self.Session()
-
-        user = User.create(session, "user1", "password")
+        user = User.create(self.session, "user1", "password")
 
         self.assertIsNotNone(user)
 
-        session.commit()
+        try:
+            self.session.commit()
+        except SQLAlchemyError:
+            self.session.rollback()
+            self.fail("failed to create user")
 
         self.assertIsNotNone(user.id)
         self.assertEqual(user.username, "user1")
@@ -35,149 +28,84 @@ class UserCreationTests(TestCase):
         self.assertIsNotNone(user.registered_at)
         self.assertIsNotNone(user.jti)
 
-        session.close()
 
-
-class UserQueryTests(TestCase):
-    def setUp(self):
-        self.engine = create_engine("sqlite:///:memory:")
-
-        Base.metadata.create_all(self.engine)
-
-        self.Session = scoped_session(sessionmaker(bind=self.engine))
-
-        session = self.Session()
-
-        self.username = "user1"
-        self.user = User.create(session, self.username, "password")
-        session.commit()
-
-        session.close()
-
-    def tearDown(self):
-        self.Session.remove()
-
+class UserQueryTests(ModelTestCaseWithMockData):
     def test_get_user_by_username(self):
-        session = self.Session()
-
-        user = User.get_by_username(session, self.username)
+        user = User.get_by_username(self.session, self.test_user_username)
 
         self.assertIsNotNone(user)
         self.assertIsNotNone(user.id)
-        self.assertEqual(user.username, self.username)
+        self.assertEqual(user.username, self.test_user_username)
         self.assertIsNotNone(user.password)
         self.assertTrue(user.active)
         self.assertIsNotNone(user.registered_at)
 
-        session.close()
 
+class userRemovalTests(ModelTestCaseWithMockData):
     def test_delete_user(self):
-        session = self.Session()
-
-        user = User.delete(session, self.username)
+        user = User.delete(self.session, self.test_user_username)
 
         self.assertIsNotNone(user)
 
-        session.commit()
+        try:
+            self.session.commit()
+        except SQLAlchemyError:
+            self.session.rollback()
+            self.fail("failed to delete user")
 
-        user = session.query(User)\
-                      .filter_by(username=self.username)\
-                      .one_or_none()
+        user = self.session.query(User)\
+                           .filter_by(username=self.test_user_username)\
+                           .one_or_none()
 
         self.assertIsNone(user)
 
 
-class UserPasswordManagementTests(TestCase):
-    def setUp(self):
-        self.engine = create_engine("sqlite:///:memory:")
-
-        Base.metadata.create_all(self.engine)
-
-        self.Session = scoped_session(sessionmaker(bind=self.engine))
-
-        session = self.Session()
-
-        self.username = "user1"
-        self.password = "password"
-        self.user = User.create(session, self.username, self.password)
-        session.commit()
-
-        session.close()
-
-    def tearDown(self):
-        self.Session.remove()
-
+class UserPasswordManagementTests(ModelTestCaseWithMockData):
     def test_change_user_password(self):
-        session = self.Session()
-
-        user = User.get_by_username(session, self.username)
+        user = User.get_by_username(self.session, self.test_user_username)
 
         original_jti = user.jti
         original_password = user.password
 
-        new_password = "new_{}".format(self.password)
+        new_password = "new_{}".format(self.test_user_password)
 
         user.change_password(new_password)
-        session.commit()
+
+        try:
+            self.session.commit()
+        except SQLAlchemyError:
+            self.session.rollback()
+            self.fail("failed to change user password")
 
         self.assertIsNotNone(user.password)
 
         self.assertNotEqual(user.password, original_password)
         self.assertNotEqual(user.jti, original_jti)
 
-        session.close()
 
-
-class UserAuthenticationTests(TestCase):
-    def setUp(self):
-        self.engine = create_engine("sqlite:///:memory:")
-
-        Base.metadata.create_all(self.engine)
-
-        self.Session = scoped_session(sessionmaker(bind=self.engine))
-
-        session = self.Session()
-
-        self.unknown_user = "unknown_user"
-        self.username = "user1"
-        self.password = "password"
-        user = User.create(session, self.username, self.password)
-        session.commit()
-
-        self.user_id = user.id
-        self.jti = user.jti
-
-        session.close()
-
-    def tearDown(self):
-        self.Session.remove()
-
+class UserAuthenticationTests(ModelTestCaseWithMockData):
     def test_authenticate_user(self):
-        session = self.Session()
-
-        user = User.authenticate(session, self.username, self.password)
+        user = User.authenticate(self.session, self.test_user_username,
+                                 self.test_user_password)
 
         self.assertIsNotNone(user)
 
     def test_fail_to_authenticate_with_invalid_password(self):
-        session = self.Session()
+        password = "invalid_{}".format(self.test_user_password)
 
-        password = "invalid_{}".format(self.password)
-
-        user = User.authenticate(session, self.username, password)
+        user = User.authenticate(self.session,
+                                 self.test_user_username, password)
 
         self.assertIsNone(user)
 
     def test_fail_to_authenticate_unknown_user(self):
-        session = self.Session()
-
-        user = User.authenticate(session, self.unknown_user, self.password)
+        user = User.authenticate(self.session, "unknown-user",
+                                 self.test_user_password)
 
         self.assertIsNone(user)
 
     def test_reset_user_token_jti(self):
-        session = self.Session()
-        user = User.get_by_username(session, self.username)
+        user = User.get_by_username(self.session, self.test_user_username)
 
         original_jti = user.jti
 
@@ -186,54 +114,28 @@ class UserAuthenticationTests(TestCase):
         self.assertNotEqual(user.jti, original_jti)
 
     def test_authenticate_using_jwt(self):
-        session = self.Session()
-
-        user = User.authenticate_using_jwt(session, self.user_id, self.jti)
+        user = User.authenticate_using_jwt(self.session, self.test_user_id,
+                                           self.test_user_jti)
 
         self.assertIsNotNone(user)
-        self.assertEqual(user.jti, self.jti)
-        self.assertEqual(user.id, self.user_id)
+        self.assertEqual(user.jti, self.test_user_jti)
+        self.assertEqual(user.id, self.test_user_id)
 
     def test_fail_to_authenticate_using_jwt_with_invalid_user_id(self):
-        session = self.Session()
-
-        user = User.authenticate_using_jwt(session, 1234, self.jti)
+        user = User.authenticate_using_jwt(self.session, 1234,
+                                           self.test_user_jti)
 
         self.assertIsNone(user)
 
     def test_fail_to_authenticate_using_jwt_with_invalid_jti(self):
-        session = self.Session()
-
         user = User.authenticate_using_jwt(
-            session, self.user_id, "invalid-jti")
+            self.session, self.test_user_id, "invalid-jti")
 
         self.assertIsNone(user)
 
 
-class JobResultTests(TestCase):
-    def setUp(self):
-        self.engine = create_engine("sqlite:///:memory:")
-
-        Base.metadata.create_all(self.engine)
-
-        self.Session = scoped_session(sessionmaker(bind=self.engine))
-
-        session = self.Session()
-
-        self.unknown_user = "unknown_user"
-        self.username = "user1"
-        self.password = "password"
-        self.user = User.create(session, self.username, self.password)
-        session.commit()
-
-        session.close()
-
-    def tearDown(self):
-        self.Session.remove()
-
+class JobResultTests(ModelTestCase):
     def test_save_job_result(self):
-        session = self.Session()
-
         job = jobs.Job(None, None, None)
         job.processed_item_count = 12
         job.failed = True
@@ -242,12 +144,16 @@ class JobResultTests(TestCase):
         end_time = start_time + timedelta(seconds=50)
         report = jobs.Report(job, start_time, end_time)
 
-        report_object = Report.save_report(session, report)
+        report_object = Report.save_report(self.session, report)
 
         self.assertIsNotNone(report_object)
         self.assertIsNone(report_object.id)
 
-        session.commit()
+        try:
+            self.session.commit()
+        except SQLAlchemyError:
+            self.session.rollback()
+            self.fail("failed to save job results")
 
         self.assertIsNotNone(report_object.id)
         self.assertEqual(report_object.job_id, job.id)
@@ -256,32 +162,10 @@ class JobResultTests(TestCase):
         self.assertEqual(report_object.num_processed_items, 12)
         self.assertTrue(report_object.failed)
 
+
+class JobRetrievalTests(ModelTestCaseWithMockData):
     def test_get_latest(self):
-        session = self.Session()
-
-        job_1 = jobs.Job(None, None, None)
-        job_1.processed_item_count = 5
-        job_1.failed = True
-        job_1.id = "job_1_uuid"
-        job_1_start_time = datetime(2016, 5, 6, 12, 0, 0)
-        job_1_end_time = job_1_start_time + timedelta(seconds=40)
-        report_1 = jobs.Report(job_1, job_1_start_time, job_1_end_time)
-
-        Report.save_report(session, report_1)
-
-        job_2 = jobs.Job(None, None, None)
-        job_2.processed_item_count = 11
-        job_2.failed = False
-        job_2.id = "job_2_uuid"
-        job_2_start_time = datetime(2016, 5, 6, 15, 0, 0)
-        job_2_end_time = job_1_start_time + timedelta(seconds=40)
-        report_2 = jobs.Report(job_2, job_2_start_time, job_2_end_time)
-
-        Report.save_report(session, report_2)
-
-        session.commit()
-
-        latest_reports = Report.get_latest(session)
+        latest_reports = Report.get_latest(self.session)
 
         self.assertEqual(len(latest_reports), 2)
 
