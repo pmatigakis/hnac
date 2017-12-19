0e4ce89fa32547d7b3e19ae5bfad8f6281809e9d
Panagiotis Matigakis <pmatigakis@gmail.com>
2017-12-19 23:35:32 +0200

    Added some more unit tests

--- tests/test_sources.py
+++ tests/test_sources.py
@@ -1,7 +1,8 @@
 from unittest import TestCase, main
 from unittest.mock import patch, MagicMock
 
-from hnac.sources import HackernewsStories
+from hnac.sources import HackernewsStories, RetryCountExceeded
+from hnac.exceptions import JobExecutionError
 
 from mock_data import story_1_data, story_2_data
 
@@ -35,6 +36,26 @@ class HackernewsStoriesItemRetrieval(TestCase):
 
         self.assertDictEqual(stories[0], story_1_data)
         self.assertDictEqual(stories[1], story_2_data)
+
+    @patch("hnac.sources.HackernewsStories._get_new_stories")
+    def test_connection_retry_exceeded_while_retrieving_new_stories(
+            self, get_new_stories_mock):
+        get_new_stories_mock.side_effect = RetryCountExceeded
+        source = HackernewsStories()
+
+        with self.assertRaises(JobExecutionError):
+            [item for item in source.items()]
+
+    @patch("hnac.sources.HackernewsStories._get_story_data")
+    @patch("hnac.sources.HackernewsStories._get_new_stories")
+    def test_received_items_are_not_stories(
+            self, get_new_stories_mock, get_story_data_mock):
+        get_new_stories_mock.return_value = [1]
+        get_story_data_mock.return_value = {"hello": "world"}
+        source = HackernewsStories()
+
+        items = [item for item in source.items()]
+        self.assertEquals(items, [])
 
 
 if __name__ == "__main__":
