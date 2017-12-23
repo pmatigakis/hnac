3695a18d2ec94483daf943b8dea875bd7e144743
Panagiotis Matigakis <pmatigakis@gmail.com>
2017-12-23 19:26:08 +0200

    Bumped version to 0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description = "Crawler for the Hackewnews API",
     author = "Panagiotis Matigakis",
     author_email = "pmatigakis@gmail.com",
-    version="0.1.0",
+    version="0.2.0",
 
     packages=find_packages(),
 
